4f64594c2c9bfb8b13f8dc3e94384877a1e7280b
edleijnse <ed@leijnse.info>
2015-05-04 20:38:00 +0200

    ResourceBundle eingebaut

--- enquiry/src/main/java/leijnse/info/enquiry/Enquiry.java
+++ enquiry/src/main/java/leijnse/info/enquiry/Enquiry.java
@@ -1,50 +1,70 @@
 package leijnse.info.enquiry;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Properties;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * Hello world!
  *
  */
 public class Enquiry {
-    private static String enquiryHeaderLocation;
-    
-    public void setEnquiryHeader(String headerLocation){
-    	enquiryHeaderLocation = headerLocation;
-    }
-	
-	public static void main(String[] args) {
-		System.out.println("Hello World Ed!");
-		Properties properties = new Properties();
-		BufferedInputStream stream;
-		
+	private static String enquiryHeaderLocation;
+
+	public void setEnquiryHeader(String headerLocation) {
+		enquiryHeaderLocation = headerLocation;
+	}
+
+	public String getEnquiryFragment(String iFragment) throws IOException {
+		String myHeader = "";
+		BufferedReader br = new BufferedReader(new FileReader(
+				iFragment));
 		try {
-			Enquiry enquiry = new Enquiry();
-			
-			stream = new BufferedInputStream(
-					new FileInputStream(
-							"C:/data/git/enquiry/enquiry/src/main/resources/enquiry.properties"));
+			StringBuilder sb = new StringBuilder();
+			myHeader = br.readLine();
 
-					properties.load(stream);
-			
-			enquiryHeaderLocation = properties.getProperty("header");
-			enquiry.setEnquiryHeader(enquiryHeaderLocation);
-			
-			System.out.println("headerLocation: " + enquiryHeaderLocation);
-
-			stream.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			while (myHeader != null) {
+				sb.append(myHeader);
+				sb.append("\n");
+				myHeader = br.readLine();
+			}
+			return sb.toString();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			br.close();
+		}
+		return myHeader;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World Ed!");
+
+		try {
+			Enquiry enquiry = new Enquiry();
+
+			// https://docs.oracle.com/javase/tutorial/displayCode.html?code=https://docs.oracle.com/javase/tutorial/i18n/resbundle/examples/PropertiesDemo.java
+
+			ResourceBundle labels = ResourceBundle.getBundle("enquiry",
+					Locale.GERMAN);
+			enquiryHeaderLocation = labels.getString("header");
+			// enquiryHeaderLocation = properties.getProperty("header");
+			enquiry.setEnquiryHeader(enquiryHeaderLocation);
+
+			System.out.println("headerLocation: " + enquiryHeaderLocation);
+			
+			String myHeader = enquiry.getEnquiryFragment(enquiryHeaderLocation);
+			
+			System.out.println("header: " + myHeader);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
 		}
 
-		
 	}
 }
