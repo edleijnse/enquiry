23d3a8e112b9c72ed41635f91af522a1616ad8ef
edleijnse <ed@leijnse.info>
2017-03-19 09:46:52 +0000

    New File Structure

---
+++ src/test/java/leijnse/info/enquery/EnquiryTest.java
@@ -0,0 +1,39 @@
+package leijnse.info.enquery;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import leijnse.info.enquiry.*;
+
+/**
+ * Unit test for simple Enquiry.
+ */
+public class EnquiryTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public EnquiryTest(String testName) {
+        super(testName);
+        System.out.println("test");
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        System.out.println("test");
+        return new TestSuite(EnquiryTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+        // Enquiry testee = new Enquiry();
+        System.out.println("test");
+
+    }
+}
